652205aa4ed6d117f7111fa60efa61a15843a661
Nicko Goodwin <33714480+nickogoodwin@users.noreply.github.com>
2022-10-26 14:02:06 -0600

    completed lab14

--- labs/lab14/ShoppingCart/index.php
+++ labs/lab14/ShoppingCart/index.php
@@ -7,7 +7,12 @@ session_set_cookie_params($lifetime, '/');
 session_start();
 
 // Create a cart array if needed
-if (empty($_SESSION['cart13'])) { $_SESSION['cart13'] = []; }
+if (empty($_SESSION['cart13'])) {
+    $cart = [];
+} else { 
+    $cart = $_SESSION['cart13']; 
+}
+
 
 // Create a table of products
 $products = [
@@ -31,17 +36,19 @@ switch($action) {
         $key = filter_input(INPUT_POST, 'productkey');
         $quantity = filter_input(INPUT_POST, 'itemqty');
         $product = $products[$key];
-        add_item($key, $quantity, $product);
+        goodwin\cart\add_item($cart, $key, $quantity, $product);
+        $_SESSION['cart13'] = $cart;
         header('Location: .?action=show_cart');
         break;
     case 'update':
         $new_qty_list = filter_input(INPUT_POST, 'newqty', 
                 FILTER_DEFAULT, FILTER_REQUIRE_ARRAY);
         foreach($new_qty_list as $key => $qty) {
-            if ($_SESSION['cart13'][$key]['qty'] != $qty) {
-                update_item($key, $qty);
+            if ($cart[$key]['qty'] != $qty) {
+                goodwin\cart\update_item($cart, $key, $qty);
             }
         }
+        $_SESSION['cart13'] = $cart;
         header('Location: .?action=show_cart');
         break;
     case 'show_cart':
@@ -51,7 +58,8 @@ switch($action) {
         include('add_item_view.php');
         break;
     case 'empty_cart':
-        unset($_SESSION['cart13']);
+        $cart = [];
+        $_SESSION['cart13'] = $cart;
         include('cart_view.php');
         break;
 }
